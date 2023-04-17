b8be93d9429f4c26ca0ae2882c0626f01b143593
John Chavez <Jrc2855@gmail.com>
2023-04-17 18:33:00 +0000

    Do not merge! Work in progress!

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require ('dotenv').config();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,14 +9,29 @@ const{ getResume } = require('./resumeGenerator');
 
 app.use(express.json());
 app.use(express.text());
+app.use(cors());
 
 // add an api to get the resume from the request body and then call resumeGenerator
+// app.post('/api/resume', async (req, res) => {
+//   console.log(req.body.toString());
+//   const resume = await getResume(req.body.toString());
+
+//   // send the resume string as a text response
+//   res.send(resume);
+// });
 app.post('/api/resume', async (req, res) => {
   console.log(req.body.toString());
   const resume = await getResume(req.body.toString());
-
+  // set the response headers to allow CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   // send the resume string as a text response
   res.send(resume);
+});
+
+app.get('/', (req, res) => {
+  res.status(200).send('Base Endpoint Proof of Life!');
 });
 
 app.listen(PORT, () => {
